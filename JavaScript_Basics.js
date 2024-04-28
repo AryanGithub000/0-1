92366fcab64c7ccd45d0c91eaa4da6825b6271e1
Aryan Singh <103366393+AryanGithub000@users.noreply.github.com>
2024-04-28 18:31:45 +0530

    Update JavaScript_Basics.js

--- JavaScript_Basics.js
+++ JavaScript_Basics.js
@@ -1,8 +1,60 @@
 //objects
 //array of objects using [] within the object
+
+//single variable has more elements in it
 const user1=[{
     firstName:"aryan",
-    gender:'male'
+    gender:"male"
+},
+{
+    firstName:"naman",
+    gender:"male"
+},{
+    firstName:"priya",
+    gender:"female",
+    metadata:{
+        age:21,
+        address:""
+    }
 }]
 
-console.log(user1);
+
+for(let i=0;i<user1.length;i++){
+    if(user1[i]["gender"]=="female"){
+        console.log(user1[i]["firstName"]);
+    }
+}
+
+
+**************************************************************************
+//Functions
+function sum(num1,num2,fnToCall){
+    let result=num1+num2;
+    //displayResult(result); one more solution
+    fnToCall(result); //callback used here
+}
+
+function displayResult(data){
+    console.log("Result of sum is: "+data);
+}
+
+function displayResultPassive(data){
+    console.log("sum result is: "+data);
+}
+
+//you can call only one function
+//how to display result?
+
+//but this uses 2 functions
+//const ans=sum(1,2);
+//displayResult(ans);
+
+//but in 1 function call??
+//change in function sum
+
+//but if i want to not change any functions then??
+const ans=sum(1,2,displayResult);
+//callbacks
+
+
+**************************************************************************
