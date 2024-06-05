0a59d22cbccdac9a8be8348c2542c900afd13bdc
Aryan Singh <103366393+AryanGithub000@users.noreply.github.com>
2024-06-05 20:37:03 +0530

    Update Callbacks in JS.js

--- Callbacks in JS.js
+++ Callbacks in JS.js
@@ -1,13 +1,47 @@
-function square(a){
-  return a*a
+function square(n) {
+  return n * n;
 }
 
-function sumOfSomething(a,b,fn){
-  const val1=fn(a); //fn called here is square for this case
-  const val2=fn(b); //this fn can be changed acc to whatever we want to callback
-  return val1+val2;
+function cube(n) {
+  return n * n * n;
 }
 
-sumOfSomething(1,2,square);
+function sumOfSquares(a, b) {
+  let square1 = square(a);
+  let square2 = square(b);
+  return square1 + square2;
+}
 
-//function in itself calling another function 
+function sumOfCubes(a, b) {
+  let square1 = cube(a);
+  let square2 = cube(b);
+  return square1 + square2;
+}
+
+let ans = sumOfCubes(1, 2);
+console.log(ans);
+
+
+//in this code we have redundancy as we are using the same logic again
+//hence we can use callbacks to make life easier 
+//callbacks => pass function as a paramter in a function
+
+//basically reduced the faltu code for each time func call
+
+function square(n) {
+  return n * n;
+}
+
+function cube(n) {
+  return n * n * n;
+}
+
+//this is generic now
+function sumOfSomething(a, b,fn) { //fn is function
+  let var1 = fn(a);
+  let var2 = fn(b);
+  return var1 + var2;
+}
+
+let ans = sumOfSomething(1, 2,square); //similarly can use cube also here
+console.log(ans);
