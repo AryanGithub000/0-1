ab8806fb7422646af450d4e77f4fdcce91da6068
Aryan Singh <103366393+AryanGithub000@users.noreply.github.com>
2024-06-20 00:38:32 +0530

    Update Node.js

---
+++ Week-2/Node.js
@@ -0,0 +1,27 @@
+//ECMA Script
+//standardized scripting language specification foundation for JS 
+//rules and regulations for scripting languages
+
+//JavaScript
+//most widely used on ECMA 
+//uses DOM not in ECMA Script specifications ***IMP***
+
+//JS Compilers -> V8 most common one -> Google Chrome bases
+//written in C and takes JS code into machine code
+
+//v8 -> open source dev by chromium browser
+//in c++
+
+//Node.js??
+//c++
+//JS meant for browser and JS ran only when browser were run
+//never meant to run in a terminal 
+//used to make JS a backend language so anyone can write it and add extra functunalities
+//Node.js is a run time
+
+//JS was not made for backend but is used now
+
+//Bun -> competition of Node.js
+//written in Zig -> v fast language
+
+
