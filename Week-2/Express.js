a41a27ad3de62f6d9a1422a2ceeca9e5f45da662
Aryan Singh <103366393+AryanGithub000@users.noreply.github.com>
2024-06-23 22:16:12 +0530

    Update Express.js

--- Week-2/Express.js
+++ Week-2/Express.js
@@ -1,8 +1,11 @@
 //Creates a HTTP server
 //go to localhost:3000 to run
+//npm init -y -> creates JSON file
 const express = require("express");
 const app = express();
 
+// / is route handler
+//headers, body, query can be added here
 app.get("/", function(request, response) {
    response.send("Hello, world!");
 });
@@ -10,3 +13,7 @@ app.get("/", function(request, response) {
 app.listen(3000, function() {
    console.log("Express app started on port 3000.");
 });
+
+
+//POSTMAN -> used to send requests
+//useful to test requests
