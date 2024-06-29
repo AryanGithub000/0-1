02c617703aa2ab28ac1aa4ac1ae13d8bf0dd77f2
Aryan Singh <103366393+AryanGithub000@users.noreply.github.com>
2024-06-30 04:43:29 +0530

    Delete Async_Await.js

--- Async_Await.js
+++
@@ -1,7 +0,0 @@
-//Aynsc await syntax
-//(Again, just syntactic sugar. Still uses callbacks/Promises under the hood)
-//makes code more readable than callbacks/promises
-
-//instead of function we add a ASYNC DECLARATION to it at the start
-//also uses AWAIT instead of .then
-//
