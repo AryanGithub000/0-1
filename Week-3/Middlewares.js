f9464135ec3f29f88b718db1abe2621209a4c7ae
Aryan Singh <103366393+AryanGithub000@users.noreply.github.com>
2024-07-26 18:45:40 +0530

    Update Middlewares.js

--- Week-3/Middlewares.js
+++ Week-3/Middlewares.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 
+//this middleware will be called everywhere
+//app.use(express.json); =>returns a function
+
+//rate limiter
+//middleware to get load of users on site 
+let numberOfRequests=0;
+function calcRequests(req,res,next){
+    numberOfRequests++;
+    console.log("Number of total visits: ",numberOfRequests);
+    next(); //this shifts control next and is imp.
+}
+
 function userMiddleware(req,res,next){
     if (!(username === "aryan" && password === "pass")) {
         res.status(403).json({ "msg": "Incorrect Inputs!" });
@@ -10,6 +22,32 @@ function userMiddleware(req,res,next){
     }
 };
 
+//Get server time to handle requests
+let totalRequests = 0;
+let totalTime = 0;
+
+function timeToHandleRequests(req, res, next) {
+    const start = Date.now();
+
+    res.on('finish', () => {
+        const duration = Date.now() - start;
+        totalRequests++;
+        totalTime += duration;
+        const avgTime = totalTime / totalRequests;
+
+        console.log(`Request URL: ${req.originalUrl}`);
+        console.log(`Time Taken: ${duration} ms`);
+        console.log(`Average Time: ${avgTime.toFixed(2)} ms`);
+    });
+
+    next();
+}
+
+
+// app.use(calcRequests);
+// app.use(timeToHandleRequests);
+//use???
+//any requests that comes to any route after this line it will be used
 
 //NAIVE APPROACH
 // app.get("/health-checkup", function(req, res) {
@@ -43,10 +81,32 @@ function userMiddleware(req,res,next){
 
 
 //MIDDLEWARE USE
-app.get("/heath-checkup",userMiddleware, function(req,res){
-    //any logic i need
+app.use(express.json());
+app.post("/health-checkup", function(req,res){
+    //kidneys =[1,2]
+    const kidneys=req.body.kidneys;
+    const kidneyLength=kidney.length;
+
+    //input validation is imp for backend
+    //as without it our files or paths can be exposed 
+    //use if else is one way
+    //the other way is to use ZOD library 
+
+
+    res.send("Your kidney len is: "+kidneyLength);
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+//global caches
+//put at the end => error handling middleware
+//if there is an excpetion
+//need 4 inputs 
+//instead of exposing our backend this is useful
+app.use(function(err,req,res,next){
+    res.json({
+        msg:"Sorry server is not working rn"
+    })
+})
+
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
 });
