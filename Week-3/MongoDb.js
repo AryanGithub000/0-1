beb0499c10806ed3ff5d9136486f412aca384209
Aryan Singh <103366393+AryanGithub000@users.noreply.github.com>
2024-08-15 03:09:59 +0530

    Update MongoDb.js

--- Week-3/MongoDb.js
+++ Week-3/MongoDb.js
@@ -1,4 +1,8 @@
+const express=require('express');
 const mongoose = require("mongoose");
+const app=express();
+
+app.use(express.json());
 
 mongoose.connect(
   "mongodb+srv://<username>:<password>@cluster0.ksc7azv.mongodb.net/user_app_new",
@@ -10,10 +14,27 @@ const User = mongoose.model("Users", {
   password: String,
 });
 
-const user = new User({
-  name: "aryan",
-  email: "aryan@gmail.com",
-  password: "123",
-});
 
-user.save();
+app.post("/signup",function(req,res){
+  const username=req.body.username;
+  const password=req.body.password;
+  const name=req.body.name;
+ 
+  const exisitngUser=User.findOne({email:username});
+  if(exisitngUser){
+    return res.status(400).send("User already exists");
+  }
+  
+  const user = new User({
+    name: name,
+    email: username,
+    password: password,
+  }); 
+
+  user.save();
+  res.json({
+    "msg":"User Created"
+  })
+})
+
+ 
